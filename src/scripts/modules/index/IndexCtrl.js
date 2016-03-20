50261c92a8af85bfa3705145f4e931cc9abba79f
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2016-03-20 12:40:37 +0100

    Got the shadow being cast based on the suns position

--- src/scripts/modules/index/IndexCtrl.js
+++ src/scripts/modules/index/IndexCtrl.js
@@ -21,17 +21,32 @@ function IndexCtrl() {
       left,
       trail;
 
+  var azimuth = getSunLocation();
+
+  // TODO offest the deg based on direction you are facing
+  castTop = -Math.sin(azimuth);
+  castLeft = -Math.cos(azimuth);
+
   for (var i = 1; i <= castDistance; i++) {
     // opacity is based on how much of the shadow is left to be cast
     percentOfNumbersUsed = ((castDistance - i) / castDistance) * 100;
     opacity = (percentOfNumbersUsed / 5000);
-    top = (castTop + i) * step;
-    left = (castLeft + i) * step;
+    // calculate using the deg the cast direction
+    top = (castTop * i) * step;
+    left = (castLeft * i) * step;
     trail = (i !== castDistance) ? ',' : '';
     shadow += 'rgba('+color+','+color+','+color+','+opacity+') '+top+'px '+left+'px 1px 3px'+trail;
   }
 
   box.css('box-shadow', shadow);
 
+}
 
+function getSunLocation() {
+  // var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
+  var position = SunCalc.getPosition(new Date(), 51.5, -0.1);
+  // console.log(position);
+  // convert the position into degrees
+  return position.azimuth;
+  // return (position.azimuth * 180 / Math.PI + 180) % 360;
 }
